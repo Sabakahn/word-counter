edeb67df83d4ff134ac251533e685802a0824e8d
Saba Samad <164026551+Sabakahn@users.noreply.github.com>
2024-06-05 02:45:46 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Display a color full wellcome message
+console.log(chalk.bold.cyanBright("\n \t\t Code With Saba-Word counter"));
+console.log("=".repeat(60));
+let answers = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter a sentence"
+    }
+]);
+let words =  answers.sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(60));
+
